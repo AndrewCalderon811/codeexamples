74f1554791773cf59d1ac0f129d42c9350805694
Andrew <111481804+AndrewCalderon811@users.noreply.github.com>
2023-09-25 19:53:34 -0700

    Update animal.cpp

--- cpp/cpp0/animal.cpp
+++ cpp/cpp0/animal.cpp
@@ -14,26 +14,38 @@ class Animal {
         void eat() { cout << "  ANIMAL " << name << " : I'm hungry!" << endl; }
 };
 
+class Turtle : public Animal {
+    private:
+        string voice;
+    public:
+        Turtle(string n="Turtle", string v="Meep")
+            : Animal(n), voice(v) {}
+
+        void speak() {
+            cout << name << ": " << voice << endl;;
+        }
+
+        void move() {
+            cout << name << ": I'm moving" << endl;
+        }
+};
+
 int main() {
-    Animal a;
-    Animal * aPtr;
-    Animal & aRef = a;
 
-    cout << "Animal object:" << endl;
-    a.speak();
-    a.move();
-    a.eat();
+    Turtle arr[3];
 
-    cout << endl << "Animal pointer:" << endl;
-    aPtr = new Animal("Dale");
-    aPtr->speak();
-    aPtr->move();
-    aPtr->eat();
+    Turtle t1;
+    Turtle t2("Michaelangelo", "COWABUNGA");
+    Turtle t3("Pistachio Disguisey", "Am I turtley enough for the turtle club?");
 
-    cout << endl << "Animal reference" << endl;
-    aRef.speak();
-    aRef.move();
-    aRef.eat();
+    arr[0] = t1;
+    arr[1] = t2;
+    arr[2] = t3;
+
+    for(int i = 0; i < 3; i++) {
+        arr[i].speak();
+        arr[i].move();
+    }
 
     return 0;
 }
